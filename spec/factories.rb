f50b23432bcae27264253d0843de95972a59053c
Ariel Parra <okinoz@gmail.com>
2012-04-10 16:42:25 -0700

    test

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+#Factory.define :user do |user|
+FactoryGirl.define do
+  factory :user, :class => User do
+    name			"Michael Hartl"
+    email			"mhartl@example.com"
+    password			"foobar"
+    password_confirmation	"foobar"
+  end
+end
