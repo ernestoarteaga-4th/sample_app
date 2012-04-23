38e9f4922ff66e76035262e6d9ef86570c1998c0
Ariel Parra <okinoz@gmail.com>
2012-04-23 10:57:40 -0700

    Sessions

---
+++ app/models/micropost.rb
@@ -0,0 +1,13 @@
+class Micropost < ActiveRecord::Base
+  self.per_page = 5
+
+  attr_accessible :content
+
+  belongs_to :user
+
+  validates :content, :presence => true, :length => { :maximum => 140 }
+  validates :user_id, :presence => true
+ 
+  default_scope :order => "microposts.created_at DESC" 
+end
+
