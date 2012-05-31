8578b34197c897604e7f5196113fc60871373fb6
Ariel Parra <okinoz@gmail.com>
2012-05-31 16:36:21 -0700

    added more files to ignore in git repository

---
+++ db/migrate/20120531164217_add_last_names_to_users.rb
@@ -0,0 +1,11 @@
+class AddLastNamesToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :last_name, :string
+    add_column :users, :second_last_name, :string
+  end
+
+  def self.down
+    remove_column :users, :last_name, :string
+    remove_column :users, :second_last_name, :string
+  end
+end
