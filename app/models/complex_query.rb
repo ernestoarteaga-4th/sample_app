f6aeefeb6de1bf248be374dd7b81eb5c3dae9da5
venegasp-armando <venegasp_armando@hotmail.com>
2013-04-22 16:12:02 -0500

    Por el reporte

---
+++ app/models/complex_query.rb
@@ -0,0 +1,17 @@
+class ComplexQuery < ActiveRecord::Base
+  
+  def self.find
+    @connector=" "
+    @tecnologies=""
+    @par = [ 1, 2, 3, 4, 5, 6, 7 ]
+    @par.each do |t|
+      @tecnologies=@tecnologies + @connector + "pt.tags_id=" + t.to_s
+      @connector=" OR "
+    end
+    @sql ="SELECT * FROM projects_tags AS pt WHERE 1=1 AND ("+ @tecnologies + ")"
+    self.connection.execute(sanitize_sql([@sql]))
+    
+  end
+  
+  
+end
