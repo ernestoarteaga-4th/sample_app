3430f0e0ff5c7bcbbd779a6cd18556325c725e59
venegasp-armando <venegasp_armando@hotmail.com>
2013-04-23 09:48:24 -0500

    Por el reporte, parte 2

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,6 +6,22 @@ class ReportController < ApplicationController
   end
 
   def search
+    
+    @connector=" "
+    @tecnologies=""
+    params[:technology].each do |t|
+      @tecnologies=@tecnologies + @connector + "ProTag.tags_id=" + t.to_s
+      @connector=" OR "
+    end
+    @sql ="SELECT Cand.* 
+      FROM candidates Cand 
+      INNER JOIN projects Proj ON Cand.id=Proj.candidate_id 
+      INNER JOIN projects_roles ProRol ON Proj.id=ProRol.projects_id 
+      INNER JOIN projects_tags ProTag ON ProRol.id=ProTag.projects_roles_id 
+      WHERE 1=1 AND ("+ @tecnologies + ")"
+    @candidates = Candidate.find_by_sql(@sql); 
+    #self.connection.execute(sanitize_sql([@sql]) 
+    
     @msg = { "success" => "true", "message" => "hello", "state" => params[:state]}
  
     respond_to do |format|
--- app/models/complex_query.rb
+++ app/models/complex_query.rb
@@ -1,5 +1,10 @@
 class ComplexQuery < ActiveRecord::Base
   
+  def initialize(param)
+    @param = param
+  end  
+  
+  
   def self.find
     @connector=" "
     @tecnologies=""
@@ -9,9 +14,6 @@ class ComplexQuery < ActiveRecord::Base
       @connector=" OR "
     end
     @sql ="SELECT * FROM projects_tags AS pt WHERE 1=1 AND ("+ @tecnologies + ")"
-    self.connection.execute(sanitize_sql([@sql]))
-    
+    self.connection.execute(sanitize_sql([@sql]) 
   end
-  
-  
 end
