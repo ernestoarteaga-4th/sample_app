2e0f968cbac9db7e598adb2ce6910106c19cbde4
venegasp-armando <venegasp_armando@hotmail.com>
2013-04-23 13:46:48 -0500

    Por el reporte, parte 3

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -6,26 +6,34 @@ class ReportController < ApplicationController
 
   def search
     
-    @connector=" "
-    @tecnologies=""
-    params[:technology].each do |t|
-      @tecnologies=@tecnologies + @connector + "ProTag.tags_id=" + t.to_s
-      @connector=" OR "
+    
+    
+    @where_tech=""
+    @from_tech=""
+    if !params[:technology].nil?
+      @connector=" "
+      params[:technology].each do |t|
+        @where_tech = @where_tech + @connector + "ProTag.tags_id=" + t.to_s
+        @connector=" OR "
+      end
+      @where_tech = "AND ("+@where_tech+")"
+      @from_tech="LEFT JOIN projects Proj ON Cand.id=Proj.candidate_id 
+        LEFT JOIN projects_roles ProRol ON Proj.id=ProRol.projects_id 
+        LEFT JOIN projects_tags ProTag ON ProRol.id=ProTag.projects_roles_id "
     end
+    
+    
+    
     @sql ="SELECT Cand.* 
-      FROM candidates Cand 
-      INNER JOIN projects Proj ON Cand.id=Proj.candidate_id 
-      INNER JOIN projects_roles ProRol ON Proj.id=ProRol.projects_id 
-      INNER JOIN projects_tags ProTag ON ProRol.id=ProTag.projects_roles_id 
-      WHERE 1=1 AND ("+ @tecnologies + ")"
+      FROM candidates Cand " + @from_tech +
+      "WHERE 1=1 "+ @where_tech 
     @candidates = Candidate.find_by_sql(@sql); 
     #self.connection.execute(sanitize_sql([@sql]) 
     
     @msg = { "success" => "true", "message" => "hello", "state" => params[:state]}
  
-    respond_to do |format|
-      format.html
-      format.json { render json: @msg }
-    end
+    @technologies = Tag.where("type_tag = 3")
+    @interview_types = InterviewsType.all
+    render :index
   end
 end
