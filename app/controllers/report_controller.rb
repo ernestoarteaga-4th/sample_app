e8b2c4368e8f7f237da29532e572596d613eaa9b
venegasp-armando <venegasp_armando@hotmail.com>
2013-04-24 15:15:21 -0500

    Report parte 4_a

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -38,14 +38,24 @@ class ReportController < ApplicationController
       event = params[:search]
       @date_from = Date.new event["date_processing_from(1i)"].to_i, event["date_processing_from(2i)"].to_i, event["date_processing_from(3i)"].to_i
       @date_to = Date.new event["date_processing_to(1i)"].to_i, event["date_processing_to(2i)"].to_i, event["date_processing_to(3i)"].to_i
-      @where_proc = " AND ( CandInt.created_at BETWEEN "+@date_from.to_s+" and "+@date_to.to_s+")"
+      @where_proc = " AND (  DATEDIFF('"+@date_from.to_s+"',CandInt.created_at) <= 0 AND DATEDIFF('"+@date_to.to_s+"',CandInt.created_at) >= 0 ) "
       @from_proc=" LEFT JOIN candidates_interviews CandInt ON Cand.id=CandInt.candidate_id "
     end
     
     
-    @sql ="SELECT Cand.* 
-      FROM candidates Cand " + @from_tech + @from_proc +
-      "WHERE 1=1 "+ @where_tech + @where_proc
+    @where_recru=""
+    @from_recru=""
+    if !params[:isRecruited].nil?
+      event = params[:search]
+      @date_hired = Date.new event["hire_date(1i)"].to_i, event["hire_date(2i)"].to_i, event["hire_date(3i)"].to_i
+      @date_start = Date.new event["start_date(1i)"].to_i, event["start_date(2i)"].to_i, event["start_date(3i)"].to_i
+      @where_recru = " AND ( DATE_FORMAT(Cand.recruited_at, '%Y-%m-%d') = '"+@date_hired.to_s+"' OR  DATE_FORMAT(Cand.started_at, '%Y-%m-%d') = '"+@date_start.to_s+"' OR Cand.recruited_in = '"+params[:search][:placeAssignment]+"'  )"
+    end
+    
+    
+    @sql ="SELECT DISTINCT Cand.* 
+      FROM candidates Cand " + @from_tech + @from_proc + @from_recru +
+      "WHERE 1=1 "+ @where_tech + @where_proc + @where_recru
     @candidates = Candidate.find_by_sql(@sql); 
     #self.connection.execute(sanitize_sql([@sql]) 
     
--- db/migrate/20130411134947_create_tags.rb
+++ db/migrate/20130411134947_create_tags.rb
@@ -7,5 +7,15 @@ class CreateTags < ActiveRecord::Migration
 
       t.timestamps
     end
+
+  Tag.create(:type_tag => 1, :name => 'NetBeans', :description => 'IDE of programation')
+  Tag.create(:type_tag => 1, :name => 'Eclipse', :description => 'IDE of programation')
+  Tag.create(:type_tag => 2, :name => 'Documentation', :description => 'Create documents of applications')
+  Tag.create(:type_tag => 2, :name => 'TeamWork', :description => 'Working in team')
+  Tag.create(:type_tag => 3, :name => 'JAVA', :description => 'Language of programation')
+  Tag.create(:type_tag => 3, :name => 'Delphi', :description => 'Language of programation')
+  Tag.create(:type_tag => 3, :name => '.NET', :description => 'Language of programation')
+  Tag.create(:type_tag => 3, :name => 'Pascal', :description => 'Language of programation')
+
   end
 end
