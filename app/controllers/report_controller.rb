6f9ddd1845b3ef85d92c9b68283a17f99a25a469
venegasp-armando <venegasp_armando@hotmail.com>
2013-04-24 12:36:20 -0500

    Report parte 4

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -32,10 +32,20 @@ class ReportController < ApplicationController
     end
     
     
+    @where_proc=""
+    @from_proc=""
+    if !params[:recruitingProcess].nil?
+      event = params[:search]
+      @date_from = Date.new event["date_processing_from(1i)"].to_i, event["date_processing_from(2i)"].to_i, event["date_processing_from(3i)"].to_i
+      @date_to = Date.new event["date_processing_to(1i)"].to_i, event["date_processing_to(2i)"].to_i, event["date_processing_to(3i)"].to_i
+      @where_proc = " AND ( CandInt.created_at BETWEEN "+@date_from.to_s+" and "+@date_to.to_s+")"
+      @from_proc=" LEFT JOIN candidates_interviews CandInt ON Cand.id=CandInt.candidate_id "
+    end
+    
     
     @sql ="SELECT Cand.* 
-      FROM candidates Cand " + @from_tech +
-      "WHERE 1=1 "+ @where_tech 
+      FROM candidates Cand " + @from_tech + @from_proc +
+      "WHERE 1=1 "+ @where_tech + @where_proc
     @candidates = Candidate.find_by_sql(@sql); 
     #self.connection.execute(sanitize_sql([@sql]) 
     
