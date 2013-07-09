8b979b7ef5879fdd2c82e971a5d33465acfa05d0
venegasp-armando <venegasp_armando@hotmail.com>
2013-07-09 12:14:45 -0500

    removed empty folders

--- server/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '9398fbf30408cf8cd49e72b5c3905d57c62b9d6b28aad9d52a55387517d88c4449b17d394c29b3090f2e2c831fe53fa8cb56589de43f65f65a4a38f955259fd2';
