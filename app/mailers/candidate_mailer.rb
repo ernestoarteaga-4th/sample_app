8dc03e725006e06e10c56017a0f250f892a6ea0c
francisco.pena <francisco.pena@4thsource.com>
2013-08-22 09:14:30 -0500

    Reset password thru email was not working, a new fix and functionality was added to cover this

---
+++ app/mailers/candidate_mailer.rb
@@ -0,0 +1,14 @@
+class CandidateMailer < ActionMailer::Base
+  default from: "jesus.parra@4thsource.com"
+
+  def reminder(candidate)
+    #@subject = 'Your login information at tweet.4thsource.com'
+    #@body["candidate"] = candidate
+    #logger.info "candidate name #{candidate.first_name}"
+    @candidate_name = candidate.first_name
+    @candidate_pass = candidate.password
+    @recipients = candidate.email
+    @from = 'Admin <jesus.parra@4thsource.com>'
+    mail(to: @recipients, subject: 'Your login information at tweet.4thsource.com')
+  end
+end
