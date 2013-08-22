5e8a1c45da71e273d45340f1c3b010e609d7d9bd
venegasp-armando <venegasp_armando@hotmail.com>
2013-08-22 08:47:15 -0500

    Admin users show all page and option to enabled/disabled an admin user

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,5 +1,22 @@
 class AdminUsersController < ApplicationController
 	def index
+    @candidates = AdminUsers.paginate(:page => params[:page], 
+                                     #:conditions => [""],
+                                     :per_page => 20)
+	end
 
+	def edit
+	    if !params.blank?
+	      @user = AdminUsers.find(params[:id])
+	      @active = @user.is_active
+	      @user.is_active = !@active	      
+	      if @user.save
+	        flash[:success] = "Profile updated."
+	      end	     
+	    end		
+	    @candidates = AdminUsers.paginate(:page => params[:page], 
+	                                     #:conditions => [""],
+	                                     :per_page => 20)	    
+	    render :update, :layout => false
 	end
 end
