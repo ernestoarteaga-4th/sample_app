9dd926d2739ab6ffc457dd896015f15ee52a4e8a
Ariel Parra <okinoz@gmail.com>
2013-08-23 16:08:44 -0500

    Fixed static user role issue and fixed styles and validacions when shown admin right menus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,31 @@ class ApplicationController < ActionController::Base
 
   helper :date
 
+  def verify_signed_in
+    unless signed_in?
+      redirect_to '/signin' if request.path != '/'
+    end
+  end
+  
+  @@admin_roles = ['Admin', 'Super Admin']
+  
+  def set_user_type(user)
+  
+    admin_user = AdminUsers.find_by_candidates_id(user.id)
+	roles = {'0' => 'Admin', '1' => 'Super Admin'}
+	
+	if admin_user
+	  session[:user_type] = roles[admin_user.lvl.to_s] 
+	else
+	   session[:user_type] = 'Candidate'
+	end
+	
+  end
+  
+  def get_user_type
+    session[:user_type]
+  end
+
   def paginate(results, per_page)
     if(params[:page].to_i>0)
       current_page = params[:page].to_i
