45474333803bcc452a7b5584835150c39e2ceb56
venegasp-armando <venegasp_armando@hotmail.com>
2013-08-26 14:20:52 -0500

    Added new link to add new users to admin/superadmin catalog

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -1,34 +1,48 @@
 class AdminUsersController < ApplicationController
+  
 	def index
-    @candidates = AdminUsers.paginate(:page => params[:page], 
+    @candidates = AdminUsers.joins(:candidate).paginate(:page => params[:page], 
                                      #:conditions => [""],
-                                     :per_page => 20)
+                                     :per_page => 20).order('candidates.first_last_name')
 	end
 
+  def new
+    unless params[:letra].nil?
+      @candidates = Candidate.where('first_last_name LIKE ?', params[:letra] + '%').order('first_last_name')
+    else
+      @candidates = Candidate.where('first_last_name LIKE "a%"').order('first_last_name')
+    end
+  end
+
+  def add
+    unless params[:id].nil?
+      @u = AdminUsers.where(:candidates_id => params[:id].to_i)
+      if @u.nil? or @u.length == 0
+        @AdminUser = AdminUsers.new()
+        @AdminUser.candidates_id = params[:id]
+        @AdminUser.is_active = 1
+        @AdminUser.lvl = 0
+        @AdminUser.save
+        render text: "User added successfuly"
+      else
+        render text: "User already exists in admin catalog "
+      end
+    else
+      render text:"Incorrect parameters"
+    end
+  end
+
 	def edit
-	    if !params.blank?
+	    unless params[:role].nil?
 	      @user = AdminUsers.find(params[:id])
         @user.lvl = params[:role].to_i
         @user.is_active = @user.lvl != 0
-	      if @user.save
-	        flash[:success] = "Profile updated."
-	      end	     
+	      @user.save
 	    end		
-	    @candidates = AdminUsers.paginate(:page => params[:page], 
-	                                     #:conditions => [""],
-	                                     :per_page => 20)	    
+	    @candidates = AdminUsers.joins(:candidate).paginate(:page => params[:page], 
+                                     #:conditions => [""],
+                                     :per_page => 20).order('candidates.first_last_name')
 	    render :update, :layout => false
 	end
-
-  def search
-    if params[:q]
-      query = params[:q]
-      @users = User.find_with_ferret(query + "*", :limit => :all)
-      @users = @users.sort_by{ |user| user.name}
-      paginate(@users, 10)
-      flash[:success] = "About #{@users.count} results."
-      render 'index'
-    end
-  end
 
 end
