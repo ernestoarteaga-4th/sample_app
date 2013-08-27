f6138d53cf1b3fc8b80dda7812f0801e3d863d7c
Ariel Parra <okinoz@gmail.com>
2013-08-27 17:46:38 -0500

    New added candidates are shown to edit instead of login

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -60,20 +60,21 @@ class CandidatesController < ApplicationController
   end
 
   def create
-=begin
+  
     #If no current_candidate, there's no login so act as new candidate.
     if !current_candidate.nil?
-      if !current_candidate.admin_flag.nil?
+      if @@admin_roles.include?( get_user_type ) #!current_candidate.admin_flag.nil?
         #algo
         @candidate = Candidate.new(params[:candidate])
         @error = @candidate.errors
         if @candidate.save  
             #Se guardo, redireccionar al edit http://localhost:3000/candidates/6/admin
-            redirect_to "/candidates/#{@candidate.id}/admin"
+            redirect_to "/candidates/#{@candidate.id}/edit" #"/candidates/#{@candidate.id}/admin"
         end
       end
+	  
     else
-=end
+	
       @candidate = Candidate.new(params[:candidate])
       @error = @candidate.errors
       if verify_recaptcha() == false
@@ -92,7 +93,8 @@ class CandidatesController < ApplicationController
           render :new
         end
       end
-    #end
+	  
+    end
 	
   end
 
