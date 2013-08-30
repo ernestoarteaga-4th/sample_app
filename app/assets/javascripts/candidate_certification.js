f0658e0543e87273de6b67a6e0d507b26846d8f9
venegasp-armando <venegasp_armando@hotmail.com>
2013-08-30 09:31:19 -0500

    Added validations

--- app/assets/javascripts/candidate_certification.js
+++ app/assets/javascripts/candidate_certification.js
@@ -1,22 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-(function($){
-window.CandidateCertification = {
-	start: function(){
-		var txtCertificationName, chBoxNotInList;
-		txtCertificationName = $("#certification_name[type=text]");
-		chBoxNotInList = $("#certification_chBNotInList");
-		txtCertificationName.attr("disabled",true);
-		chBoxNotInList.click(function(){
-			if(chBoxNotInList.is(":checked")){
-				txtCertificationName.attr("disabled",false);
-			}
-			else{
-				txtCertificationName.attr("disabled",true);
-			}
-		});
-	}
-
-};
-
-})(jQuery);
+$(document).ready(function() {
+	$("#certification_chBNotInList").change(function() {
+	    if(this.checked) {
+	      $("#certification_name").removeAttr('readonly');	
+	    }
+	    else {
+	    	$("#certification_name").val('');
+	    	$("#certification_name").prop('readonly', 'true');
+	    }	    	
+	 });
+});
